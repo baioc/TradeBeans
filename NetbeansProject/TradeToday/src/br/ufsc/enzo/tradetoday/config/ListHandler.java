2ec8a334c933270d71edccb1e918ed9f004628fb
Enzo Albornoz <enzocoelhoalbornoz@hotmail.com>
2018-10-30 17:55:58 -0200

    Trying to fix list

--- NetbeansProject/TradeToday/src/br/ufsc/enzo/tradetoday/config/ListHandler.java
+++ NetbeansProject/TradeToday/src/br/ufsc/enzo/tradetoday/config/ListHandler.java
@@ -5,8 +5,13 @@
  */
 package br.ufsc.enzo.tradetoday.config;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.Reader;
 import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
@@ -22,9 +27,14 @@ public class ListHandler {
     private static String[][] list = null;
     private static String selectedType = DEFAULT_TYPE;
     
+    private static java.io.BufferedReader a;
+    
     public static void updateList() {
         try {
-            java.io.Reader reader = null;
+            String path = "/home/100000000901519/";
+            String name = "stocks.csv";
+            java.io.Reader reader = java.nio.file.Files.newBufferedReader(
+                    java.nio.file.Paths.get(path,name));
             CSVParser p = new CSVParser(reader,
                         CSVFormat.DEFAULT
                                 .withHeader("symbol",
@@ -33,18 +43,23 @@ public class ListHandler {
                                             "desc")
                                 .withIgnoreHeaderCase()
                                 .withTrim());
-            list = new String[p.getRecords().size()][4];
+            ArrayList<String[]>aList = new ArrayList<String[]>();
             for(CSVRecord r : p){
-                int i = (int) r.getRecordNumber();
-                list[i][0] = r.get("symbol");
-                list[i][1] = r.get("type");
-                list[i][2] = r.get("name");
-                list[i][3] = r.get("desc");
+                String[] line = new String[4];
+                line[0] = r.get("symbol");
+                line[1] = r.get("type");
+                line[2] = r.get("name");
+                line[3] = r.get("desc");
+                aList.add(line);
             }
-            for(int i = 0;i < list.length;i++){
+            list = new String[aList.size()][4];
+            for(int z = 0;z < aList.size();z++){
+                list[z] = aList.get(z);
+            }
+            for(int k = 1;k < list.length;k++){
                 System.out.printf("Symbol: %s | Type : %s | Name : %s | "
-                        + "Desc: %s \n",list[i][0],list[i][1],
-                        list[i][2],list[i][3]);
+                        + "Desc: %s \n",list[k][0],list[k][1],
+                        list[k][2],list[k][3]);
             }
         } catch(IOException e){
             System.err.println(e.getMessage());
@@ -79,12 +94,26 @@ public class ListHandler {
         for(int i = 0;i < list.length;i++){
             if(list[i][1].equals(TYPE_STOCK)){
                 symbols.add(list[i][0]);
+                
             }
         }
-        return symbols.toArray(list[0]);
+        String[] r = new String[symbols.size()];
+        for(int i = 0;i < r.length;i++){
+            r[i] = symbols.get(i);
+        }
+        return r;
     }
     
-    public static void main(String [] args){
-        
+    public static void main(String [] args) throws FileNotFoundException{
+        String[] c = getSymbolsCripto();
+        String[] s = getSymbolsStock();
+        for(String l : c){
+            System.out.println(l);
+            System.out.println("a");
+            
+        }
+        for(String l : s){
+            System.out.println(l);
+        }
     }
 }
